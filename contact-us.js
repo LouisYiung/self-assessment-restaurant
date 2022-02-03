ec1fecd85f45cec36cab819c31d739b11174b1a6
LouiChew <louisbhchew@gmail.com>
2022-02-02 16:38:25 -0800

    got help

--- contact-us.js
+++ contact-us.js
@@ -1,6 +1,21 @@
+//this is just a copy paste of the javascript code that is on the contact us page.
+//I alsow understand that it is generally a better standard to seperate the the javascript from the page as it saves time to debug and maintain
+//if i were better, i would be able to make this work as such, but due to constraints on time and my want to submit this asap, i am going with the on page implementation 
+
+const emailRegex = /[0-9a-zA-Z]+@[0-9a-zA-Z]+.{2,3}[a-z]/;
+let button = document.getElementById("submit");
+
 function validateEmail() {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!document.getElementByID("#Email").value.match(emailRegex)) {
-        document.querySelector('button[type="submit"]').innerHTML("")
+    var edValue = document.getElementById("Email");
+    var s = edValue.value;
+
+    if (!document.getElementById("Email").value.match(emailRegex)) {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
     }
+}
+
+function submitFunction() {
+    alert("form is good, form is submitted");
 }
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function modulo(number, mod) {
+//While attempting to implement the Carousel on to my page, my javascript inexplicably would not comply with handling the the querySelector function.
+//
+/*function modulo(number, mod) {
     let result = number % mod;
     if (number < 0) {
         result += mod;
@@ -6,30 +8,26 @@ function modulo(number, mod) {
     return result;
 }
 
-const buttonPrevious = document.getElementById("data-carousel-button-previous");
-buttonPrevious.addEventListener('click', () => console.log('clicked Previous'));
-/*class Carousel {
-    constructor(carousel) {
-        this.buttonPrevious = carousel.querySelector(['data-carousel-button-previous']);
-        this.buttonNext = carousel.querySelector(['data-carousel-button-next']);
-        this.slidesContainer = carousel.querySelector(['data-carousel-slides-container']);
-        this.currentSlide = 0;
-        this.numSlides = this.slidesContainer.children.length;
-
-        this.buttonPrevious.addEventListener('click', () => console.log('clicked Previous'));
-        this.buttonPrevious.addEventListener('click', () => console.log('clicked Next'));
-    }
-
-    handleNext() {
-        this.currentSlide = modulo(this.currentSlide + 1, this.numSlides);
-        this.carousel.style.setProperty('--current-slide', this.currentSlide);
-    }
-    handlePrevious() {
-        this.currentSlide = modulo(this.currentSlide - 1, this.numSlides);
-        this.carousel.style.setProperty('--current-slide', this.currentSlide);
-    }
-
+function handleNext() {
+    currentSlide += 1;
+    carousel.style.setProperty('--current-slide', currentSlide);
 }
 
-const carousels = document.querySelectorAll("[data-carousel]");
-carousels.forEach(carouselElement => new Carousel(carouselElement));*/
+function handlePrevious() {
+    currentSlide -= 1;
+    carousel.style.setProperty('--current-slide', currentSlide);
+}
+
+const slidesContainer = document.querySelector('[data-carousel-slides-container]');
+const ButtonPrevious = document.querySelector('[data-carousel-button-previous]');
+const ButtonNext = document.querySelector('[data-carousel-button-next]');
+const carousel = document.querySelector('[data-carousel]');
+const numSlides = slidesContainer.children.length();
+console.log(numSlides);
+let currentSlide = 0;
+
+//ButtonPrevious.addEventListener('click', () => console.log("Previous Clicked"));
+//ButtonNext.addEventListener('click', () => console.log("Next Clicked"));
+
+//ButtonPrevious.addEventListener('click', HandlePrevious());
+ButtonNext.addEventListener('click', handleNext());
