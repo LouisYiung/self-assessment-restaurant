861683432369797e4a6bf2d81048d7b3607c6f77
LouiChew <louisbhchew@gmail.com>
2022-02-02 16:43:51 -0800

    hopefully this code works

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //While attempting to implement the Carousel on to my page, my javascript inexplicably would not comply with handling the the querySelector function.
 //
-/*function modulo(number, mod) {
+function modulo(number, mod) {
     let result = number % mod;
     if (number < 0) {
         result += mod;
@@ -9,12 +9,12 @@
 }
 
 function handleNext() {
-    currentSlide += 1;
+    currentSlide += modulo(currentSlide + 1, numSlides);
     carousel.style.setProperty('--current-slide', currentSlide);
 }
 
 function handlePrevious() {
-    currentSlide -= 1;
+    currentSlide += modulo(currentSlide - 1, numSlides);
     carousel.style.setProperty('--current-slide', currentSlide);
 }
 
@@ -22,12 +22,8 @@ const slidesContainer = document.querySelector('[data-carousel-slides-container]
 const ButtonPrevious = document.querySelector('[data-carousel-button-previous]');
 const ButtonNext = document.querySelector('[data-carousel-button-next]');
 const carousel = document.querySelector('[data-carousel]');
-const numSlides = slidesContainer.children.length();
-console.log(numSlides);
+const numSlides = slidesContainer.children.length;
 let currentSlide = 0;
 
-//ButtonPrevious.addEventListener('click', () => console.log("Previous Clicked"));
-//ButtonNext.addEventListener('click', () => console.log("Next Clicked"));
-
-//ButtonPrevious.addEventListener('click', HandlePrevious());
+ButtonPrevious.addEventListener('click', handlePrevious());
 ButtonNext.addEventListener('click', handleNext());
