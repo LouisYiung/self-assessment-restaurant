5eaea34391c20634a6ec9010e4e8cb27bcd36c50
LouiChew <louisbhchew@gmail.com>
2022-02-05 10:10:35 -0800

    changes to contact  javascript

--- contact-us.js
+++ contact-us.js
@@ -1,12 +1,13 @@
  // i do understand this regex is not exhaustive and disallows emails such as ....@...com and excludes lots of punctuations and other characters, but i feel for an assignment such as this; this regex is enough
  const emailRegex = /[0-9a-zA-Z]+@[0-9a-zA-Z]+.{2,3}[a-z]/;
- let button = document.getElementById("submit");
+ const emailInput = document.querySelector("[data-Email]");
+ const submitButton = document.querySelector('[data-Submit]');
+ const resetButton = document.querySelector("[data-Reset]");
 
  function validateEmail() {
-     var edValue = document.getElementById("Email");
+     var edValue = document.querySelector("data-Email");
      var s = edValue.value;
-     console.log("submit was pressed")
-     if (!document.getElementById("Email").value.match(emailRegex)) {
+     if (s.value.match(emailRegex)) {
          alert("this email is not valid.");
      } else {
          alert("this email is valid.")
@@ -24,11 +25,10 @@
  }*/
 
  function resetForm() {
-     var inputs, index;
-
-     inputs = document.getElementsByTagName('input');
-     for (index = 0; index < inputs.length; ++index) {
-         console.log(index);
-         //inputs[index].value = false;
+     var inputs = document.getElementsByTagName('input');
+     for (var i = 0; i < inputs.length; i += 1) {
+         inputs[i].value = '';
      }
  }
+
+ submitButton.addEventListener('click', validateEmail);
