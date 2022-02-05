c67397e6eef1e1f448b304d975547b202b04b873
LouiChew <louisbhchew@gmail.com>
2022-02-05 09:40:49 -0800

    finally got everything to work

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-//While attempting to implement the Carousel on to my page, my javascript inexplicably would not comply with handling the the querySelector function.
-//
-/*function modulo(number, mod) {
+const slidesContainer = document.querySelector('[data-carousel-slides-container]');
+const ButtonPrevious = document.querySelector('[data-carousel-button-previous]');
+const ButtonNext = document.querySelector('[data-carousel-button-next]');
+const carousel = document.querySelector('[data-carousel]');
+const numSlides = slidesContainer.children.length;
+let currentSlide = 0;
+
+function modulo(number, mod) {
     let result = number % mod;
     if (number < 0) {
         result += mod;
@@ -9,21 +14,19 @@
 }
 
 function handleNext() {
-    currentSlide += modulo(currentSlide + 1, numSlides);
+    currentSlide = modulo(currentSlide + 1, numSlides);
     carousel.style.setProperty('--current-slide', currentSlide);
+    console.log(currentSlide);
 }
 
 function handlePrevious() {
-    currentSlide += modulo(currentSlide - 1, numSlides);
+    currentSlide = modulo(currentSlide - 1, numSlides);
     carousel.style.setProperty('--current-slide', currentSlide);
+    console.log(currentSlide);
 }
 
-const slidesContainer = document.querySelector('[data-carousel-slides-container]');
-const ButtonPrevious = document.querySelector('[data-carousel-button-previous]');
-const ButtonNext = document.querySelector('[data-carousel-button-next]');
-const carousel = document.querySelector('[data-carousel]');
-const numSlides = slidesContainer.children.length;
-let currentSlide = 0;
 
-ButtonPrevious.addEventListener('click', handlePrevious());
-ButtonNext.addEventListener('click', handleNext());*/
+ButtonPrevious.addEventListener('click', handlePrevious);
+ButtonNext.addEventListener('click', handleNext);
+
+console.log(currentSlide);
